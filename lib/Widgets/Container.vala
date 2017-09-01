elementary/wingpanel

commit 94c7c9a9a712c3683814c14ee793a7e07974f21e

Container.vala: use GObject style (#45)

lib/Widgets/Container.vala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2015 Wingpanel Developers (http://launchpad.net/wingpanel)
+ * Copyright (c) 2011-2017 elementary LLC (https://elementary.io)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
@@ -18,22 +18,16 @@
  */
 
 public class Wingpanel.Widgets.Container : Gtk.Button {
-    private Gtk.Grid content_widget;
+    public Gtk.Grid content_widget { get; construct; }
 
     public Container () {
-        content_widget = new Gtk.Grid ();
+        Object (content_widget: new Gtk.Grid ());
+    }
+
+    construct {
         content_widget.hexpand = true;
+        add (content_widget);
 
-        this.add (content_widget);
-
-        set_style_classes ();
-    }
-
-    public Gtk.Grid get_content_widget () {
-        return content_widget;
-    }
-
-    private void set_style_classes () {
         var style_context = this.get_style_context ();
         style_context.add_class (Gtk.STYLE_CLASS_MENUITEM);
         style_context.remove_class (Gtk.STYLE_CLASS_BUTTON);

lib/Widgets/Button.vala
@@ -23,8 +23,6 @@ public class Wingpanel.Widgets.Button : Container {
     private Gtk.Image button_image;
 
     public Button (string caption, string? icon_name = null) {
-        var content_widget = this.get_content_widget ();
-
         button_image = create_image_for_icon_name (icon_name);
         button_label = create_label_for_caption (caption);
 
@@ -33,8 +31,6 @@ public class Wingpanel.Widgets.Button : Container {
     }
 
     public Button.with_mnemonic (string caption, string? icon_name = null) {
-        var content_widget = this.get_content_widget ();
-
         button_image = create_image_for_icon_name (icon_name);
         button_label = create_label_for_caption (caption, true);
 

lib/Widgets/Switch.vala
@@ -25,8 +25,6 @@ public class Wingpanel.Widgets.Switch : Container {
     public new signal void switched ();
 
     public Switch (string caption, bool active = false) {
-        var content_widget = this.get_content_widget ();
-
         button_label = create_label_for_caption (caption);
         button_switch = create_switch (active);
 
@@ -37,8 +35,6 @@ public class Wingpanel.Widgets.Switch : Container {
     }
 
     public Switch.with_mnemonic (string caption, bool active = false) {
-        var content_widget = this.get_content_widget ();
-
         button_label = create_label_for_caption (caption, true);
         button_switch = create_switch (active);
 

commit 56665704f74cadbdaeb89c280f3f0ac9eca93aaf

container: Avoid ABI/API break caused by 94c7c9a9a712c3683814c14ee793a7e07974f21e

lib/Widgets/Container.vala
@@ -18,7 +18,9 @@
  */
 
 public class Wingpanel.Widgets.Container : Gtk.Button {
-    public Gtk.Grid content_widget { get; construct; }
+    public Gtk.Grid content_widget { owned get; construct; }
+
+    public extern Gtk.Grid get_content_widget ();
 
     public Container () {
         Object (content_widget: new Gtk.Grid ());