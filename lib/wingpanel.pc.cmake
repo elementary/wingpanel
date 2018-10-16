987dd59b47b758e1d4c198f84ad024c4e1e194a8
Corentin Noël <corentin@elementary.io>
2018-10-16 19:32:44 +0200

    Show the right library version with pkgconfig

--- lib/wingpanel.pc.cmake
+++ lib/wingpanel.pc.cmake
@@ -6,7 +6,7 @@ indicatorsdir=${libdir}/wingpanel
  
 Name: Wingpanel
 Description: Wingpanel headers
-Version: @LIB_VERSION@
+Version: @VERSION@
 Libs: -l@LIB_NAME@
 Cflags: -I${includedir}/@LIB_NAME@
 Requires: glib-2.0 gee-0.8 gmodule-2.0 gtk+-3.0
