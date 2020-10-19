d80354412225da3bc349d0db49e08d07936259a5
David Hewitt <davidmhewitt@gmail.com>
2020-10-19 21:45:16 +0100

    Fix indentation

--- src/is_wayland.c
+++ src/is_wayland.c
@@ -2,10 +2,9 @@
 #include "gdk/gdkwayland.h"
 
 int is_wayland () {
-	if (GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
-		return 1;
-	} else {
-		return 0;
-	}
+    if (GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ())) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
-
